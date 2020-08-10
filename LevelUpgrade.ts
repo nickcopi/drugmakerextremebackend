4adf58e6e5111229a0b369c7bd8201b4d21d699a
nick <nick@noemail4unonon.comlol>
2020-08-10 15:07:37 -0400

    Fix levelup upgrade description.

--- LevelUpgrade.ts
+++ LevelUpgrade.ts
@@ -2,7 +2,7 @@ import { Upgrade } from "./Upgrade";
 
 export class LevelUpgrade extends Upgrade {
     constructor(onBuy: (()=>void)) {
-        super('Level Up', 'Unlocks new dealers and allows for better drug', 10);
+        super('Level Up', 'Unlocks new dealers and allows for better drug crafting.', 10);
         this.onBuy = onBuy;
     }
 }
